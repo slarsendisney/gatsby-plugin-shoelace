da12db4266d1588956ba97a4c03d01034a2b1c4a
slarsendisney <s.larsendisney@gmail.com>
2020-07-28 12:26:58 +0100

    testing via-npm

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from "react"
-import { Helmet } from "react-helmet"
+import React from "react";
+import { Helmet } from "react-helmet";
 
 export const wrapPageElement = ({ element }) => (
   <>
     <Helmet>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.8/dist/shoelace/shoelace.css"
-      />
-      <script
-        type="module"
-        src="https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.8/dist/shoelace/shoelace.esm.js"
-      ></script>
+      <link rel="stylesheet" href="/shoelace.css" />
+      <script type="module" src="/shoelace.esm.js"></script>
     </Helmet>
     {element}
   </>
-)
+);
