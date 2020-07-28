2637cfb5d85ce68c0a4c9b9cc742d8e379c8b23f
slarsendisney <s.larsendisney@gmail.com>
2020-07-28 14:38:31 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-
+import React from "react";
+import { Helmet } from "react-helmet";
+import { withPrefix } from "gatsby";
 export const wrapPageElement = ({ element }) => (
   <>
     <Helmet>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.8/dist/shoelace/shoelace.css"
-      />
-      <script
-        type="module"
-        src="https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.8/dist/shoelace/shoelace.esm.js"
-      ></script>
+      <script src={withPrefix("shoelace.esm.js")} type="module" />
     </Helmet>
     {element}
   </>
-)
+);
